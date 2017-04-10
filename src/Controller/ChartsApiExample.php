d2ebfd3d5388ec4c4285eb32f2c95677a87a433b
andileco <daniel@andile.co>
2017-04-10 11:43:38 -0400

    Making the language more generic

--- src/Controller/ChartsApiExample.php
+++ src/Controller/ChartsApiExample.php
@@ -20,16 +20,17 @@ class ChartsApiExample extends ControllerBase implements ContainerInjectionInter
     $library = $this->chartSettings['library'];
     $options = [];
     $options['type'] = $this->chartSettings['type'];
-    $options['title'] = 'Chart title';
-    $options['yaxis_title'] = '';
+    $options['title'] = t('Chart title');
+    $options['yaxis_title'] = t('Y-Axis');
     $options['yaxis_min'] = '';
     $options['yaxis_max'] = '';
+    $options['xaxis_title'] = t('X-Axis');
     //sample data format
-    $categories = ["attachUganda","attachKenya","Kenya","Uganda"];
+    $categories = ["Category 1","Category 2","Category 3","Category 4"];
     $seriesData = [
-      ["name" => "subaru", "color" => "#0d233a", "type" => null, "data" => [250, 350, 400, 200]],
-      ["name" => "Nissan", "color" => "#8bbc21", "type" => "column", "data" => [150, 450, 500, 300]],
-      ["name" => "Toyota", "color" => "#910000", "type" => "area",  "data" => [0, 0, 60, 90]]
+      ["name" => "Series 1", "color" => "#0d233a", "type" => null, "data" => [250, 350, 400, 200]],
+      ["name" => "Series 2", "color" => "#8bbc21", "type" => "column", "data" => [150, 450, 500, 300]],
+      ["name" => "Series 3", "color" => "#910000", "type" => "area",  "data" => [0, 0, 60, 90]]
     ];
 
     $element = array(
